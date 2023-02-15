eb9d697247f934300160f48552e75da2fe7ab571
amresh prasad <amresh04061999@gmail.com>
2023-02-15 22:27:29 +0530

    feat:footer design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-const routes: Routes = [{ path: 'contact-manage', loadChildren: () => import('./contact-manage/contact-manage.module').then(m => m.ContactManageModule) },];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'contact-manage'
+  },
+  {
+    path: 'contact-manage', loadChildren: () => import('./contact-manage/contact-manage.module').then(m => m.ContactManageModule)
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
@@ -10,7 +11,9 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
-    CoreModule
+    AppRoutingModule,
+    CoreModule,
+  
   ],
 
   providers: [],
