ea4bd9681152f0fa2175ab276f435838b09ba3bb
amresh prasad <amresh04061999@gmail.com>
2023-02-15 23:29:17 +0530

    feat:list-created

--- src/app/contact-manage/contact-manage-routing.module.ts
+++ src/app/contact-manage/contact-manage-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactListContainerComponent } from './contact-list-container/contact-list-container.component';
 import { ContactManageComponent } from './contact-manage.component';
 
-const routes: Routes = [{ path: '', component: ContactManageComponent }];
+const routes: Routes = [{
+  path: '', component: ContactManageComponent,
+  children: [{
+    path: '',
+    pathMatch: 'full',
+    redirectTo:'employee-list'
+  },
+    {
+      path: "employee-list", component: ContactListContainerComponent
+    }
+  
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
